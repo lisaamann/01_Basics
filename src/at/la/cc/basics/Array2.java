49e7ee1c01f7b4c06cd339c34fe5fe9fdc9f0656
lisaamann <liisa.amann@gmail.com>
2021-03-22 21:25:27 +0100

    add ignore files

--- src/at/la/cc/basics/Array2.java
+++ src/at/la/cc/basics/Array2.java
@@ -8,14 +8,19 @@ public class Array2 {
         //Gib alle Zahlen in einem For-Loop aus und berechne gleichzeitig die Summe der Zahlen.
 
         Random random = new Random();
-        int RandomNr = random.nextInt(100);
-        int[] randomNumber = new int[50];//mit dem array merke ich mir was ich 50 mal ausgegeben habe
-        System.out.println(RandomNr);
+        int[] randomNumber = new int[50];
+        int summe = 0;
 
-        for (int i = 0; i < 50; i++) {
-            System.out.println(RandomNr);
+
+        for (int i = 0; i < randomNumber.length; i++) {
+            int randomNumber2 = random.nextInt(100);
+            randomNumber[i] = randomNumber2;
+
+            summe = summe + randomNumber[i];
+
+        }
+        System.out.println(summe);
 
 
         }
     }
-}
--- src/at/la/cc/basics/PersonenImLift.java
+++
@@ -1,9 +0,0 @@
-package at.la.cc.basics;
-
-public class PersonenImLift {
-    public static void main(String[] args) {
-        //Erstelle ein Zahlenarray dass das Gewicht von Personen in einem Aufzug beinhaltet.
-        //Mit Hilfe einer Schleife werden die einzelnen Werte addiert, sollte das maximal zulässige Gesamtgewicht
-        //von 1.600 kg überschritten werden, so wird ausgegeben das der Aufzug überlastet ist und die Schleife beendet sich.
-    }
-}
