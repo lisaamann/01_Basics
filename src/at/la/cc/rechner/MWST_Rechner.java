c6f22a8fe56c0b81947c0c0dd6c91999cefdfc26
lisaamann <liisa.amann@gmail.com>
2021-05-31 20:59:20 +0200

    commit

--- src/at/la/cc/rechner/MWST_Rechner.java
+++ src/at/la/cc/rechner/MWST_Rechner.java
@@ -1,5 +1,62 @@
 package at.la.cc.rechner;
 
 public class MWST_Rechner {
+    public int mehrwertssteuer;
+    public String Laenderkuerzel;
+    public String Land;
 
+    //region CONSTRUCTOR
+    public MWST_Rechner(int mehrwertssteuer, String laenderkuerzel, String land) {
+        this.mehrwertssteuer = mehrwertssteuer;
+        Laenderkuerzel = laenderkuerzel;
+        this.Land = Land;
+    }
+    //endregion
+
+    //region METHODEN
+    public void setLandByUserInput(){
+        this.Land = Land;
+        this.Laenderkuerzel = Laenderkuerzel;
+        if (Laenderkuerzel.equals("CH")){
+            setLand("Schweiz");
+            setMehrwertssteuer(10);
+        }
+        if (Laenderkuerzel.equals("GB")){
+            setLand("Großbritanien");
+            setMehrwertssteuer(15);
+        }
+        if (Laenderkuerzel.equals("DE")){
+            setLand("Deutschland");
+            setMehrwertssteuer(19);
+        }
+    }
+
+    //endregion
+
+
+    //region GETTER SETTER
+    public int getMehrwertssteuer() {
+        return mehrwertssteuer;
+    }
+
+    public void setMehrwertssteuer(int mehrwertssteuer) {
+        this.mehrwertssteuer = mehrwertssteuer;
+    }
+
+    public String getLaenderkuerzel() {
+        return Laenderkuerzel;
+    }
+
+    public void setLaenderkuerzel(String laenderkuerzel) {
+        Laenderkuerzel = laenderkuerzel;
+    }
+
+    public String getLand() {
+        return Land;
+    }
+
+    public void setLand(String land) {
+        Land = land;
+    }
+    //endregion
 }
--- src/at/la/cc/rechner/MainRechner.java
+++ src/at/la/cc/rechner/MainRechner.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class MainRechner {
     public static void main(String[] args) {
         Rechner rechner1 = new Rechner(10, 5);
-        Waehrungsrechner waehrungsrechner1 = new Waehrungsrechner("€",10);
+        Waehrungsrechner waehrungsrechner1 = new Waehrungsrechner("€", 10);
+        MWST_Rechner mwst_rechner1 = new MWST_Rechner(10, "CH", "Schweiz");
         Scanner userInput = new Scanner(System.in);
 
+        //region Grundrechnen
         System.out.println("Willkommen beim Grundrechnen");
         System.out.println("Bitte geben Sie Ihre erste Zahl ein");
         double userInputRechner = userInput.nextDouble();
@@ -17,19 +19,21 @@ public class MainRechner {
         rechner1.setZahl2(userInput2Rechner);
         System.out.println("Bitte geben Sie den Rechenoperator ein");
         String userInputRechner2 = userInput.next();
-        if(userInputRechner2.equals("+")){
+        if (userInputRechner2.equals("+")) {
             System.out.println("Das Ergebnis ist: " + rechner1.addieren(rechner1.getZahl(), rechner1.getZahl2()));
         }
-        if (userInputRechner2.equals("-")){
+        if (userInputRechner2.equals("-")) {
             System.out.println("Das Ergebnis ist: " + rechner1.subtrahieren(rechner1.getZahl(), rechner1.zahl2));
         }
-        if (userInputRechner2.equals("/")){
+        if (userInputRechner2.equals("/")) {
             System.out.println("Das Ergebnis ist: " + rechner1.dividieren(rechner1.zahl, rechner1.zahl2));
         }
-        if (userInputRechner2.equals("*")){
+        if (userInputRechner2.equals("*")) {
             System.out.println("Das Ergebnis ist " + rechner1.multiplizieren(rechner1.zahl, rechner1.zahl2));
         }
+        //endregion
 
+        //region Währungsrechnen
         System.out.println("Bitte geben Sie den Betrag in Euro ein den Sie umrechnen wollen");
         double userInputWaehrung = userInput.nextDouble();
         waehrungsrechner1.setBetrag(userInputWaehrung);
@@ -37,6 +41,16 @@ public class MainRechner {
         String userInputWaehrung2 = userInput.next();
         waehrungsrechner1.setWaehrung(userInputWaehrung2);
         System.out.println(waehrungsrechner1.Umgerechnet(userInputWaehrung, userInputWaehrung2));
+        //endregion
+
+        //region Mehrwertssteuer
+        System.out.println("Bitte geben Sie das Länderkürzel ein für das Sie die Mehrwertssteuer wissen wollen.");
+        String userInputMehrwertssteuer = userInput.next();
+        mwst_rechner1.setLaenderkuerzel(userInputMehrwertssteuer);
+        mwst_rechner1.setLandByUserInput();
+        System.out.println("Die Mehrwertssteuer in " + mwst_rechner1.getLand() + " beträgt " + mwst_rechner1.getMehrwertssteuer() + "%");
+
+        //endregion
 
     }
 }
