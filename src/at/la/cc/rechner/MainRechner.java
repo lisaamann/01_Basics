5ea714a2c5f8d3f433da328f5067855c79038d6a
lisaamann <liisa.amann@gmail.com>
2021-05-31 20:20:49 +0200

    commit

--- src/at/la/cc/rechner/MainRechner.java
+++ src/at/la/cc/rechner/MainRechner.java
@@ -1,7 +1,33 @@
 package at.la.cc.rechner;
 
+import java.util.Scanner;
+
 public class MainRechner {
     public static void main(String[] args) {
+        Rechner rechner1 = new Rechner(10, 5);
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Willkommen beim Grundrechnen");
+        System.out.println("Bitte geben Sie Ihre erste Zahl ein");
+        double userInputRechner = userInput.nextDouble();
+        rechner1.setZahl(userInputRechner);
+        System.out.println("Bitte geben Sie Ihre zweite Zahl ein");
+        double userInput2Rechner = userInput.nextDouble();
+        rechner1.setZahl2(userInput2Rechner);
+        System.out.println("Bitte geben Sie den Rechenoperator ein");
+        String userInputRechner2 = userInput.next();
+        if(userInputRechner2.equals("+")){
+            System.out.println("Das Ergebnis ist: " + rechner1.addieren(rechner1.getZahl(), rechner1.getZahl2()));
+        }
+        if (userInputRechner2.equals("-")){
+            System.out.println("Das Ergebnis ist: " + rechner1.subtrahieren(rechner1.getZahl(), rechner1.zahl2));
+        }
+        if (userInputRechner2.equals("/")){
+            System.out.println("Das Ergebnis ist: " + rechner1.dividieren(rechner1.zahl, rechner1.zahl2));
+        }
+        if (userInputRechner2.equals("*")){
+            System.out.println("Das Ergebnis ist " + rechner1.multiplizieren(rechner1.zahl, rechner1.zahl2));
+        }
 
     }
 }
--- src/at/la/cc/rechner/Rechner.java
+++ src/at/la/cc/rechner/Rechner.java
@@ -4,27 +4,22 @@ import java.util.Scanner;
 
 public class Rechner {
     public double zahl = 10;
-    public char addieren = '+';
-    public char subtrahieren = '-';
-    public char multiplizieren = '*';
-    public char dividieren = '/';
-    Scanner scanner = new Scanner(System.in);
+    public double zahl2 = 5;
 
     //region CONSTRUCTOR
-    public Rechner(int zahl) {
+
+    public Rechner(double zahl, double zahl2) {
         this.zahl = zahl;
+        this.zahl2 = zahl2;
     }
     //endregion
 
     //region METHODEN
-    public double rechnung(){
-        this.zahl = zahl;
 
-        return zahl;
-    }
     //endregio
 
     //region GETTER SETTER
+
     public double getZahl() {
         return zahl;
     }
@@ -33,44 +28,32 @@ public class Rechner {
         this.zahl = zahl;
     }
 
-    public Scanner getScanner() {
-        return scanner;
+    public double getZahl2() {
+        return zahl2;
     }
 
-    public void setScanner(Scanner scanner) {
-        this.scanner = scanner;
+    public void setZahl2(double zahl2) {
+        this.zahl2 = zahl2;
     }
 
-    public char getAddieren() {
-        return addieren;
+    public double addieren(double zahl, double zahl2) {
+        double ergebnis = zahl + zahl2;
+        return ergebnis;
     }
 
-    public void setAddieren(char addieren) {
-        this.addieren = addieren;
+    public double multiplizieren(double zahl, double zahl2) {
+        double ergebnis = zahl * zahl2;
+        return ergebnis;
     }
 
-    public char getSubtrahieren() {
-        return subtrahieren;
+    public double subtrahieren(double zahl, double zahl2){
+        double ergebnis = zahl - zahl2;
+        return ergebnis;
     }
 
-    public void setSubtrahieren(char subtrahieren) {
-        this.subtrahieren = subtrahieren;
-    }
-
-    public char getMultiplizieren() {
-        return multiplizieren;
-    }
-
-    public void setMultiplizieren(char multiplizieren) {
-        this.multiplizieren = multiplizieren;
-    }
-
-    public char getDividieren() {
-        return dividieren;
-    }
-
-    public void setDividieren(char dividieren) {
-        this.dividieren = dividieren;
+    public double dividieren(double zahl, double zahl2){
+        double ergebnis = zahl / zahl2;
+        return ergebnis;
     }
     //endregion
 }
